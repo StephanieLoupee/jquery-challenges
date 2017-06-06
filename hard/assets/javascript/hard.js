98965a172cb3f2693a51d3c53fb558f591ca07bb
Stephanie <stephanie.loupee@libertymutual.com>
2017-06-05 21:59:01 -0400

    notes only on hard

--- hard/assets/javascript/hard.js
+++ hard/assets/javascript/hard.js
@@ -35,6 +35,32 @@
    //code in here wont run until page loads
    $(function(){
 
+    //  * 1. Any time someone clicks on the title at the top "feed template,"
+    //  *    make the color of the "feed template" text change from black to red
+    //  *    and then from red to black when clicked again.
+
+//This is going to be an on click, change the style text color
+// with the complicating factor of needing to know which color you are now,
+// so that the "other" color can be assigned.
+
+    //  * 2. The links on the side of the page – "your name," "section 1," "section 2," etc. –
+    //  *    hide every odd-numbered link in the menu.
+
+//Need to dig for this one - hide
+
+    //  * 3. Change every instance of the word "bacon" on the page to be "LASER VISION"
+
+//Looks like a global replace command is in order.  Need to research
+
+    //  * 4. Delete the last two posts in the middle section (they have a CSS class "post"
+
+
+
+    //  * 5. Remove the images in the right column
+
+    //  * BONUS: add a special surprise inside!
+
+
 
 
    })
